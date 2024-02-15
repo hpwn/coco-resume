de6480bcc4d7cd80f89431445fb13e6b0c40b43a
Hayden Price <hwplivnlow@gmail.com>
2024-02-14 17:20:57 -0700

    Implement basic gpt-3.5-turbo-0125 functionality

--- scripts/parse_job_description.py
+++ scripts/parse_job_description.py
@@ -1,4 +1,5 @@
-import openai
+from openai import OpenAI
+
 from dotenv import load_dotenv
 import os
 
@@ -7,15 +8,23 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Set the API key for the OpenAI API calls
-openai.api_key = openai_api_key
+client = OpenAI(api_key=openai_api_key)
 
 
 # Define the function to call the OpenAI API using GPT-3
-def gpt3_call(prompt, engine="davinci-002", max_tokens=100):
-    response = openai.Completion.create(
-        engine=engine, prompt=prompt, max_tokens=max_tokens
+def gpt3_call(prompt, model="gpt-3.5-turbo-0125", max_tokens=100):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=max_tokens,
     )
-    return response.choices[0].text.strip()
+    if response.choices:
+        return response.choices[0].message.content.strip()
+    else:
+        return "No response generated."
 
 
 # Function to parse the job description and extract relevant skills and technologies
